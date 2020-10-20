12e9e981301f797dc54da84bbca4d30d1ff1be6a
soliakindrat99 <48919489+soliakindrat99@users.noreply.github.com>
2020-10-20 12:52:50 +0300

    add some requests to database (CREATE, UPDATE, DELETE goods)

--- src/controllers/goods_controller.js
+++ src/controllers/goods_controller.js
@@ -10,3 +10,51 @@ exports.get_goods = (req, res) => {
         })
         .then((user) => res.json(user));
 };
+
+exports.create_goods = (req, res) => {
+    goods.create(req.body)
+        .then((goods) => res.json(goods))
+        .catch((err) => {
+            res.status(400).send({ message: err.message() || "Body can not be empty!!!" });
+        });
+};
+
+exports.delete_goods = (req, res) => {
+    goods.destroy({
+            where: { id: req.params.id }
+        })
+        .then((result) => {
+            if (result == 1) {
+                res.send({
+                    message: "The goods was successfully deleted!"
+                });
+            } else {
+                res.send({
+                    message: "The goods could not be deleted!"
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message || "Delete error" });
+        });
+};
+
+exports.update_goods = (req, res) => {
+    goods.update({
+            where: { id: req.params.id }
+        })
+        .then((result) => {
+            if (result == 1) {
+                res.send({
+                    message: "The goods was successfully updated!"
+                });
+            } else {
+                res.send({
+                    message: "The goods could not be updated!"
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message || "Update error" });
+        });
+};
--- src/routes/goods.js
+++ src/routes/goods.js
@@ -3,5 +3,8 @@ const goods_controller = require('../controllers/goods_controller.js');
 
 router.get('/', goods_controller.all_goods);
 router.get('/:id', goods_controller.get_goods);
+router.post('/', goods_controller.create_goods);
+router.delete('/:id', goods_controller.delete_goods);
+router.put('/:id', goods_controller.update_goods);
 
 module.exports = router;
